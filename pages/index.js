8047bf58082384943510a7b2f130b82e03a9f3ef
Adnan <adnan172203@gmail.com>
2020-07-15 08:27:15 +0600

    fetch new stories data from hacker news

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,43 @@
+import React, {useState,useEffect} from 'react';
+import fetch from 'isomorphic-fetch';
 
-const Index = () => {
+const Index = ({allpost}) => {
+
+    const [post,setPost] = useState([]);
+    
+    useEffect(()=>{
+        setPost(allpost);
+    },[]);
+
     return (
         <div>
             <h1>Hello hacker news</h1>
+            { post.map(p => <p key={p.id}>{p.title}</p>) }
         </div>
     )
 }
 
+
+
+export async function getStaticProps() {
+    const res = await fetch('https://hacker-news.firebaseio.com//v0/newstories.json?print=pretty');
+    const data = await res.json();
+    const statuscode = res.status > 200 ? res.status : false;
+
+
+    const promises = data.slice(0, 20).map(id => fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(
+        response => response.json()
+      )
+    );
+  const allpost = await Promise.all(promises);
+
+
+    console.log(allpost);
+    return {
+        props: {
+            allpost,
+            statuscode,
+        },
+    };
+}
 export default Index
