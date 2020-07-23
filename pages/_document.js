b5f35ddaa8b707cd7145a252ce11c3cfd559396c
Adnan <adnan172203@gmail.com>
2020-07-23 16:23:36 +0600

    changed some document page code

--- pages/_document.js
+++ pages/_document.js
@@ -1,28 +1,22 @@
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
   }
+
   render() {
     return (
-      <html lang='en-US'>
-        <Head>
-          <meta
-            name='description'
-            content='hacker news clone'
-          />
-          <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-          <meta name='viewport' content='width=device-width' />
-        </Head>
+      <Html>
+        <Head />
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
-    );
+      </Html>
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
